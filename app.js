a8835ac7e78bb05c94927e4d3a65c94407fe1ef2
rolandeke <rolandeke49@gmail.com>
2020-04-12 23:42:07 +0000

    Removed some unnecessary console log statements and changed some tags in my email message format

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('/contact', (req,res) => {
       from: process.env.EMAIL,
       to: process.env.TO,
       subject: "New Mail From Your Portfolio",
-      html: `<h4>Sender: ${name}</h4><br> <h4>Email: ${email} </h4><br> <h5>${message}</h5>`,
+      html: `<h4>Sender: ${name}</h4><h>Email: ${email} </h><p class="lead">${message}</p>`,
     };
 
     transporter.sendMail(mailOptions, function(err, info) {
@@ -65,12 +65,10 @@ app.post('/contact', (req,res) => {
           `There was an error sending your message please try again. Thank you!`
         );
         res.redirect("/contact");
-        console.log(err);
       } 
       else{
         req.flash('success', `Dear ${name}, your message was sent successfully.I will get back to you as soon as possible, Thank you for reaching out.`)
         res.redirect('/contact')
-        //console.log(info);
       } 
     });
    
