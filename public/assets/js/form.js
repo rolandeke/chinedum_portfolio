3629b1f56df6b4d570577ec0d6794a4d614326dc
rolandeke <rolandeke49@gmail.com>
2020-11-01 05:52:24 +0000

    Made some changes to the website, changed the font added more projects to my portfolio section

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -1,21 +1,26 @@
 $(document).ready(function () {
-
-    $("#contact-form").validate({
-        messages: {
-
-            name: {
-                required: 'Please enter your name' 
-            },
-            email: {
-                required: 'Please enter your email' 
-            },
-            message: {
-                required: 'Please enter your message' 
-            }
-
-        }
-
-    });
-
-
+  //   $(".btn-filter").click(function () {
+  //     var val = $(this).attr("data-filter");
+  //     console.log(val);
+  //     $("#proj-list li").each(function () {
+  //       if ($(this).classList.contains(val) > -1) {
+  //         $(this).show();
+  //       } else {
+  //         $(this).hide();
+  //       }
+  //     });
+  //   });
+  $("#contact-form").validate({
+    messages: {
+      name: {
+        required: "Please enter your name",
+      },
+      email: {
+        required: "Please enter your email",
+      },
+      message: {
+        required: "Please enter your message",
+      },
+    },
+  });
 });
